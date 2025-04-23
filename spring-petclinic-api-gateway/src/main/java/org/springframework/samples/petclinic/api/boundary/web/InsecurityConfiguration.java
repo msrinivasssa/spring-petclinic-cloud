687d8266f5304b49833a6a1ec6b23eb16270d449
Madhavan <msrinivasssa@vmware.com>
2025-04-23 22:52:19 +0530

    updating spring-cloud-bindings dependency

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/boundary/web/InsecurityConfiguration.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/boundary/web/InsecurityConfiguration.java
@@ -2,24 +2,20 @@ package org.springframework.samples.petclinic.api.boundary.web;
 
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebFluxSecurity
+@Configuration
 public class InsecurityConfiguration {
-    //@formatter:off
+
     @Bean
-    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
-            )
-            .csrf(AbstractHttpConfigurer::disable)
-            .httpBasic(Customizer.withDefaults());
-
+            );
         return http.build();
     }
 }
